231c1ac7999da1da0b1c205c989d1de98d33894a
xuannguyen1206 <haxuannnguyenn@gmail.com>
2022-03-20 14:53:23 +0700

    _

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache  } from '@apollo/client';
 
 const apolloClient = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: "https://travel-world-graphql.herokuapp.com/",
   ssrMode: true,
   defaultOptions:{
     query:{
