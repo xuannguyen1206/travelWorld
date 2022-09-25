17b3095ac9d57e4db24675cac8d0f042f1532e13
xuannguyen1206 <haxuannnguyenn@gmail.com>
2022-09-26 01:42:53 +0700

    finish page /countries

--- src/pages/countries/index.tsx
+++ src/pages/countries/index.tsx
@@ -1,30 +1,115 @@
+import Image from 'next/image';
 import { useEffect, useState } from 'react'
 import Navbar from '../../components/navbar';
-import styles from '../../styles/404.module.scss'
+import styles from '../../styles/countries.module.scss'
+import sunset from '../../asset/sunset2.jpg'
+import testImg from "../../asset/test.jpg"
+import testImg2 from "../../asset/test2.jpg"
+import testImg3 from "../../asset/test3.jpg"
+import axios from 'axios';
+/* @ts-ignore */
+import Fade from 'react-reveal/Fade';
+import { ApolloClient, gql, InMemoryCache } from '@apollo/client';
+import Link from 'next/link';
 
-export default function Custom404() {
-  const [dot,setDot] = useState<any>('.');
+interface Country{
+  name: string
+  picture: Pictures
+}
+interface Pictures{
+  landscape: [Pic]
+}
+interface Pic{
+  link:string
+  blur:string
+}
 
-  function addDot(){
-    setDot((dot: string)=> {
-      if(dot === '...'){
-        setDot('.')
-      } else {
-        setDot(dot+'.')
-      }
-    })
+
+export default function countries({countryData}:{countryData:[Country]}) {
+  const [showImg,setShowImg] = useState(false);
+  const [quote,setQuote] = useState('');
+  const [author,setAuthor] = useState('');
+  const quotesKeywords = ['freedom','life','inspiration']
+  function onOffImg(){
+    setShowImg(!showImg);
   }
-
-  useEffect(()=> { 
-    setInterval(addDot,2000)
+  async function getQuote() {
+    const { data } = await axios.get('https://quotable.io/random?tags=inspirational|life|freedom')
+    setAuthor(data.author);
+    setQuote(data.content);
+    console.log(data)
+  }
+  useEffect(()=>{
+    getQuote();
+    setTimeout(()=> {
+      onOffImg()
+    },3500)
   },[])
-  
   return (
   <main className={styles.main}>
-    <div className={styles.nav}>
+      <figure className={styles.bgImg}/>
       <Navbar/>
-    </div>
-    <h1>CCC {dot}</h1>
+      <section className={styles.quote}>
+        { (author && quote) &&
+          <Fade when={showImg}>
+            <p>
+              {`${quote}  — ${author}`}
+            </p>
+          </Fade>
+        }
+      </section>
+      <Fade bottom cascade when={showImg}>
+        <section className={styles.countryCards}>
+          {countryData.map((country) => {
+            return (
+                <div className={styles.card}>
+                  <Link href={`/countries/${country.name}`}>
+                    <figure className={styles.countryPic}>
+                      <Image src={country.picture.landscape[0].link} layout='fill' objectFit='cover'
+                        placeholder='blur' blurDataURL={country.picture.landscape[0].blur}/>
+                      <figcaption>{country.name}</figcaption>
+                    </figure>
+                  </Link>
+                </div>
+            )
+          })}
+        </section>
+      </Fade>
+
   </main>
   );
+}
+
+export async function getServerSideProps(){
+  const client = new ApolloClient({
+    uri: "https://travel-world-graphql.herokuapp.com/",
+    ssrMode: true,
+    defaultOptions:{
+      query:{
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'all'
+      }
+    },
+    cache: new InMemoryCache()
+  });
+  const { data } = await client.query({
+    query: gql`
+      query Countries{
+        countries(quantity: 6) {
+          name
+          picture {
+            landscape(quantity: 1) {
+              link
+              blur
+            }
+          }
+        }
+      }
+    `
+  })
+  return{
+    props: {
+      countryData:data.countries,
+    }
+  }
 }
